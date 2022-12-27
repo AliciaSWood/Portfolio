2f2c308a64392f422ccca0d89c447b059e53969d
Alicia Wood <aliciaw_silvie@outlook.com>
2022-12-27 11:26:50 +1030

    header

--- react-intro/src/containers/Projects/Projects.jsx
+++ react-intro/src/containers/Projects/Projects.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Element } from "react-scroll"
-import styles from "./Projects.module.scss"
+import React from "react";
+import { Element } from "react-scroll";
+import styles from "./Projects.module.scss";
 
 const Projects = () => {
-    return (
-        <Element name = "Projects">
-        <div className = {styles.Projects}>
-            
+  return (
+    <Element name="Projects">
+      <div className={styles.Projects}>
+        <div className={styles.Header}>
+          <div className={styles.waviy}>
+            <span className={styles.waviySpan}>P</span>
+            <span className={styles.waviySpan}>R</span>
+            <span className={styles.waviySpan}>O</span>
+            <span className={styles.waviySpan}>J</span>
+            <span className={styles.waviySpan}>E</span>
+            <span className={styles.waviySpan}>C</span>
+            <span className={styles.waviySpan}>T</span>
+            <span className={styles.waviySpan}>S</span>
+          </div>
         </div>
-        </Element>
-    );
+      </div>
+    </Element>
+  );
 };
 
 export default Projects;
