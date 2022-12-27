08d3534fd5be00b768922b308d7a4766bd8856f6
Alicia Wood <aliciaw_silvie@outlook.com>
2022-12-27 18:14:55 +1030

    about-me started

--- react-intro/src/containers/AboutMe/AboutMe.jsx
+++ react-intro/src/containers/AboutMe/AboutMe.jsx
@@ -6,7 +6,13 @@ const AboutMe = () => {
     return (
         <Element name = "AboutMe">
         <div className = {styles.AboutMain}>
-
+            <div className = {styles.profileBox}>
+        <img className = {styles.image} src = {require("../../assets/self-image.png")}></img>
+        <div className = {styles.bio}>
+        <p>I'm Alicia Wood, a junior software developer from Adelaide. My passion for tech and coding comes partially from my love of puzzles, as I've always enjoyed the challenge of solving larger problems by tackling them one piece at a time. <br></br>Another part of my passion for tech comes from its limitless capacity for new and brilliant things. We've made such incredible advancements in technology and it's thrilling to be a part of that, even if it is as just a small piece of a larger puzzle.</p>
+        </div>
+        </div>
+        <img className = {styles.clouds} src = {require("../../assets/clouds.png")}></img>
         </div>
         </Element>
     );
